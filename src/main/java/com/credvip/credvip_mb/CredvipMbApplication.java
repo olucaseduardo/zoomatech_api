8fa6d76da466e7001a21ade0651f26ebbff62c83
Lucas Eduardo Lima da Silva <lucaspity721@gmail.com>
2026-01-29 09:35:05 -0300

    build: initialize project

---
+++ src/main/java/com/credvip/credvip_mb/CredvipMbApplication.java
@@ -0,0 +1,13 @@
+package com.credvip.credvip_mb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CredvipMbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CredvipMbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/credvip/credvip_mb/CredvipMbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.credvip.credvip_mb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CredvipMbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
