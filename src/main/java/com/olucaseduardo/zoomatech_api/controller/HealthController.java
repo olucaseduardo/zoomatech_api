420b335fa4deab414b8e2be96dc2abd275ea50ac
lucaseduardo <lucaspity721@gmail.com>
2026-05-03 08:40:16 -0300

    feature: adjust health request for query database too

--- src/main/java/com/olucaseduardo/zoomatech_api/controller/HealthController.java
+++ src/main/java/com/olucaseduardo/zoomatech_api/controller/HealthController.java
@@ -1,16 +1,22 @@
 package com.olucaseduardo.zoomatech_api.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/health")
+@RequiredArgsConstructor
 public class HealthController {
 
-    @GetMapping
-    public ResponseEntity<String> check() {
-        return ResponseEntity.ok("UP");
+    private final JdbcTemplate jdbcTemplate;
+
+    @RequestMapping(method = {RequestMethod.GET, RequestMethod.HEAD})
+    public ResponseEntity<Void> check() {
+        jdbcTemplate.queryForObject("SELECT 1", Integer.class);
+        return ResponseEntity.ok().build();
     }
 }
