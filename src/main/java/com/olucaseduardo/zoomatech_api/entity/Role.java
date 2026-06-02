803158d2376adc29092a81649baa42cb9b1f11c5
lucaseduardo <le.eduardo.dev@gmail.com>
2026-06-02 10:51:32 -0300

    fix: rename table role to roles to avoid PostgreSQL reserved word conflict

--- src/main/java/com/olucaseduardo/zoomatech_api/entity/Role.java
+++ src/main/java/com/olucaseduardo/zoomatech_api/entity/Role.java
@@ -12,7 +12,7 @@ import java.sql.Timestamp;
 import java.util.UUID;
 
 @Entity
-@Table(name = "role")
+@Table(name = "roles")
 @Data
 @Builder
 @NoArgsConstructor
---
+++ src/main/resources/db/migration/V5__rename_role_to_roles.sql
@@ -0,0 +1,3 @@
+ALTER TABLE member DROP CONSTRAINT fk_member_role;
+ALTER TABLE role RENAME TO roles;
+ALTER TABLE member ADD CONSTRAINT fk_member_role FOREIGN KEY (role_id) REFERENCES roles (id);
